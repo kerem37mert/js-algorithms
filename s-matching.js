8cdab86edc8d6a7c63a0d687484a8b303f97f5dd
Kerem <kerem37mert@yahoo.com>
2024-11-03 16:25:41 +0300

    string-matching updated

--- s-matching.js
+++ s-matching.js
@@ -1,12 +1,23 @@
-const text = "Hi, it is a text";
-const word = "its";
+const text = "Herhangi bir yardım almadan linear search pseudocode yazınız.";
+const word = "e";
 
-for(var i=0; i<= text.length - word.length; i++) {
-    var j = 0;
+const stringMatching = (text, word) => {
+    const indexes = [];
 
-    while(j<word.length && word[j] == text[i+j])
-        j++;
+        for(let i=0; i<text.length - word.length; i++) {
+            let j = 0;
+            while(j<word.length && word[j] == text[i+j])
+                j++;
 
-    if(j == word.length)
-        console.log("kelime bulundu");
-}
+            if(j == word.length)
+                indexes.push(i); 
+        }
+
+    return indexes;
+} 
+
+result = stringMatching(text, word);
+if(!result.length)
+    console.log("Aranan kelime bulunamadı.");
+else
+    console.log(result);
